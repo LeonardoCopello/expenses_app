3fbe5cdb91b500fdb7fedc9bdb4c9078d20bbbd8
LeonardoCopello <93166418+LeonardoCopello@users.noreply.github.com>
2022-03-04 10:33:32 -0300

    App Expenses

---
+++ lib/components/chart_bar.dart
@@ -0,0 +1,62 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_constructors_in_immutables
+
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  final String? label;
+  final double? value;
+  final double? percentage;
+
+  ChartBar({
+    this.label,
+    this.value,
+    this.percentage,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          height: 20,
+          child: FittedBox(
+            child: Text(
+              '${value?.toStringAsFixed(2)}',
+            ),
+          ),
+        ),
+        SizedBox(height: 5),
+        SizedBox(
+          height: 60,
+          width: 10,
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(label.toString()),
+      ],
+    );
+  }
+}
